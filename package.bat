5ceac991d0ba37e68bb69287f280fea97c33812e
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2026-07-17 18:23:02 -0400

    finalizing polyhook2

--- package.bat
+++ package.bat
@@ -4,3 +4,5 @@ copy /y "%cd%\bin\Release\x64\winhttp.dll" "output"
 md %cd%\output\mio-mod-loader
 copy /y "%cd%\mio-mod-loader\bin\Release\net8.0\MioModLoader.dll" "%cd%\output\mio-mod-loader"
 copy /y "%cd%\mio-mod-loader\bin\Release\net8.0\MioModLoader.runtimeconfig.json" "%cd%\output\mio-mod-loader"
+copy /y "%cd%\mio-mod-loader\bin\Release\net8.0\PolyHook2.Net.dll" "%cd%\output\mio-mod-loader"
+xcopy "%cd%\mio-mod-loader\bin\Release\net8.0\runtimes\win-x64" "%cd%\output\mio-mod-loader\runtimes\win-x64" /E /I /H /C
