267e925ac72642944497b69338108624fe6f0f51
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2026-09-09 18:59:37 -0400

    add install instructions

--- package.bat
+++ package.bat
@@ -7,3 +7,4 @@ copy /y "%cd%\mio-mod-loader\bin\Release\net8.0\MioModLoader.runtimeconfig.json"
 copy /y "%cd%\mio-mod-loader\bin\Release\net8.0\PolyHook2.Net.dll" "%cd%\output\mio-mod-loader"
 xcopy "%cd%\mio-mod-loader\bin\Release\net8.0\runtimes\win-x64" "%cd%\output\mio-mod-loader\runtimes\win-x64" /E /I /H /C
 copy /y "%cd%\libs\MioBinds.dll" "%cd%\output\mio-mod-loader"
+copy /y "%cd%\INSTALL.txt" "%cd%\output"
